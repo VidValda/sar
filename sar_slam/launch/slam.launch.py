7074052d20f14327070d2c0fc0dca7ab9d5833fa
VidValda <valdajosevid@gmail.com>
2026-05-26 18:12:31 +0200

    feat(sar_slam): update SLAM configuration and add laser filter node to launch

--- sar_slam/launch/slam.launch.py
+++ sar_slam/launch/slam.launch.py
@@ -1,5 +1,6 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
+from launch.conditions import UnlessCondition
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
@@ -21,6 +22,18 @@ def generate_launch_description():
         'use_sim_time', default_value='false', description='Use simulation/Gazebo clock'
     )
 
+    laser_filter_config = PathJoinSubstitution(
+        [FindPackageShare('rosbot_utils'), 'config', 'rosbot_xl', 'laser_filter.yaml']
+    )
+
+    laser_filter_node = Node(
+        package='laser_filters',
+        executable='scan_to_scan_filter_chain',
+        name='laser_filter',
+        parameters=[laser_filter_config],
+        condition=UnlessCondition(use_sim_time),
+    )
+
     slam_node = Node(
         package='slam_toolbox',
         executable='async_slam_toolbox_node',
@@ -28,4 +41,4 @@ def generate_launch_description():
         parameters=[slam_params_file, {'use_sim_time': use_sim_time}],
     )
 
-    return LaunchDescription([use_sim_time_arg, slam_params_file_arg, slam_node])
+    return LaunchDescription([use_sim_time_arg, slam_params_file_arg, laser_filter_node, slam_node])
